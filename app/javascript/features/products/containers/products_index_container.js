4f4c9dbca9f2976c68666ce88197c89a1982d042
Shinsuke Nishio <nishio@densan-labs.net>
2017-06-25 16:44:11 +0900

    fix typo

--- app/javascript/features/products/containers/products_index_container.js
+++ app/javascript/features/products/containers/products_index_container.js
@@ -65,7 +65,7 @@ const mapDispatchToProps = dispatch => ({
       product_name: {
         filter: { value: ''}
       },
-      variant_name: {
+      name: {
         filter: { value: ''}
       },
       price: {
